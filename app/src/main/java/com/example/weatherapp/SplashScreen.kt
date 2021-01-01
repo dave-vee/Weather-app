1e7a210c6d8ebb84d508134b47f2f89bde12bdb3
dave-vee <davemwangiee@gmail.com>
2021-01-01 19:44:14 +0300

    added a splashscreen

--- app/src/main/java/com/example/weatherapp/SplashScreen.kt
+++ app/src/main/java/com/example/weatherapp/SplashScreen.kt
@@ -8,62 +8,16 @@ import kotlinx.android.synthetic.main.splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
 
-    private val SPLASH_DELAY: Long = 1000 //3 seconds
-    private var mDelayHandler: Handler? = null
-    private var progressBarStatus = 0
-    var dummy: Int = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
-        mDelayHandler = Handler()
-        //Navigate with delay
-        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3500)
     }
 
-
-    private fun launchMainActivity() {
-        var intent = Intent(this, MainActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-        startActivity(intent)
-        this.finish()
-        mDelayHandler!!.removeCallbacks(mRunnable)
-
-    }
-
-    private val mRunnable: Runnable = Runnable {
-
-        Thread(Runnable {
-            while (progressBarStatus < 100) {
-                // performing some dummy operation
-                try {
-                    dummy = dummy + 25
-                    Thread.sleep(100)
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
-                }
-                // tracking progress
-                progressBarStatus = dummy
-
-                // Updating the progress bar
-                splash_screen_progress_bar.progress = progressBarStatus
-            }
-
-            //splash_screen_progress_bar.setProgress(10)
-
-            launchMainActivity()
-
-
-        }).start()
-    }
-
-    override fun onDestroy() {
-
-        if (mDelayHandler != null) {
-            mDelayHandler!!.removeCallbacks(mRunnable)
-        }
-
-        super.onDestroy()
-    }
 
 }
+
